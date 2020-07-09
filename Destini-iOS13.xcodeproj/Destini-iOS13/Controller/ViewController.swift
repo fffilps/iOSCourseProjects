5661f7a6e6702fdf1a90c15cda009edf5ab1197a
fffilps <60311148+fffilps@users.noreply.github.com>
2020-07-09 11:45:04 -0500

    The Dependent Files for the Project

---
+++ Destini-iOS13.xcodeproj/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    var storyCount = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle!
+        _ = storyBrain.checkAnswer(userChoice)
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        let choice1 = storyBrain.getChoice1()
+        let choice2 = storyBrain.getChoice2()
+        storyLabel.text = storyBrain.getText()
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
+    }
+}
+
