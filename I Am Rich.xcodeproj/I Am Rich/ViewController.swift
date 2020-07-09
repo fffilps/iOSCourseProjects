260e5331076feb61df2bdf87ceb7498797a76077
fffilps <60311148+fffilps@users.noreply.github.com>
2020-07-09 11:36:57 -0500

    The Dependent Files for the Project

---
+++ I Am Rich.xcodeproj/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jmari Wyatt on 5/11/20.
+//  Copyright © 2020 Jmari Wyatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
