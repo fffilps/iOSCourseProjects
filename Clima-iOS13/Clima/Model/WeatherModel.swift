01a9433ebbbe2ede4332d30388783d16419c183e
fffilps <60311148+fffilps@users.noreply.github.com>
2020-07-09 12:06:57 -0500

    A Weather app that uses an API
    Its a weather fetching app, that uses an API from the web. It is my first app using API's and URLSessions to let my app connect to the web.

---
+++ Clima-iOS13/Clima/Model/WeatherModel.swift
@@ -0,0 +1,48 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Spliff Watson on 7/4/2 R.
+//  Copyright © 2 Reiwa App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let condtionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+        
+    
+        
+    }
+    
+    var conditionName: String {
+            switch condtionId {
+            case 200...232:
+                return "cloud.bolt"
+            case 300...321:
+                return "cloud.drizzle"
+            case 500...531:
+                return "cloud.rain"
+            case 600...622:
+                return "cloud.snow"
+            case 701...781:
+                return "cloud.fog"
+            case 800:
+                return "sun.max"
+            case 801...804:
+                return "cloud.bolt"
+            default:
+                return "cloud"
+            }
+
+        }
+
+    }
+    
+    
+
