ebd4acd9c04b8dd4581c0180c1f299ffe7109b1c
fffilps <60311148+fffilps@users.noreply.github.com>
2020-07-09 11:32:30 -0500

    Add files via upload

---
+++ Quizzler-iOS13.xcodeproj/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
+
+    var timer = Timer()
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        
+        quizBrain.nextQuestion()
+        
+        
+        //this is calling the timer and also making it wait however long to call the function #selected in #selector... (THIS CALLS THE FUNCTION ALSO)
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI() {
+        
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        otherButton.backgroundColor = UIColor.clear
+     
+        let getAnswer = quizBrain.getAnswerOptions()
+        trueButton.setTitle(getAnswer[0], for: .normal)
+        falseButton.setTitle(getAnswer[1], for: .normal)
+        otherButton.setTitle(getAnswer[2], for: .normal)
+            
+        
+    }
+    
+}
+
