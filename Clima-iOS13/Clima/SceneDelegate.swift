afc046141a32f01437445094b62de297f25903f1
J'mari Wyatt <60311148+fffilps@users.noreply.github.com>
2020-07-15 14:21:02 -0500

    Clima App. A Live Weather App

--- Clima-iOS13/Clima/SceneDelegate.swift
+++ Clima-iOS13/Clima/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  Clima
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
+//  Created by J'mari Wyatt on 01/09/2019.
+//  Copyright © 2019 J'mariko Consulting. All rights reserved.
 //
 
 import UIKit
