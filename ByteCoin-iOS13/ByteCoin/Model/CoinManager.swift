18feac340315139c65dd3d9617d2668444810a9a
J'mari Wyatt <60311148+fffilps@users.noreply.github.com>
2020-07-15 13:30:13 -0500

    ByteCoin App. Checks Bitcoin Prices

--- ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -2,8 +2,8 @@
 //  CoinManager.swift
 //  ByteCoin
 //
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by J'mari Wyatt on 11/09/2019.
+//  Copyright © 2019 J'mariko Consulting. All rights reserved.
 //
 
 import Foundation
