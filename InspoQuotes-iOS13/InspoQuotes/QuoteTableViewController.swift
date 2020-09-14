2b58b8c7095b07f0a19d5589b58672b3b87cb560
J'mari Wyatt <60311148+fffilps@users.noreply.github.com>
2020-09-14 11:50:58 -0500

    InspoQuotes App. An App of Inspirational Quotes

---
+++ InspoQuotes-iOS13/InspoQuotes/QuoteTableViewController.swift
@@ -0,0 +1,166 @@
+//
+//  QuoteTableViewController.swift
+//  InspoQuotes
+//
+//  Created by J'mari Wyatt on 18/07/2020.
+//  Copyright © 2020 J'mariko Consulting. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class QuoteTableViewController: UITableViewController, SKPaymentTransactionObserver {
+    
+    
+    let productID = "com.jmariwyatt.InspoQuotes.PremiumQuotes"
+    
+    var quotesToShow = [
+        "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
+        "All our dreams can come true, if we have the courage to pursue them. – Walt Disney",
+        "It does not matter how slowly you go as long as you do not stop. – Confucius",
+        "Everything you’ve ever wanted is on the other side of fear. — George Addair",
+        "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+        "Hardships often prepare ordinary people for an extraordinary destiny. – C.S. Lewis"
+    ]
+    
+    let premiumQuotes = [
+        "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine. ― Roy T. Bennett",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. – Nelson Mandela",
+        "There is only one thing that makes a dream impossible to achieve: the fear of failure. ― Paulo Coelho",
+        "It’s not whether you get knocked down. It’s whether you get up. – Vince Lombardi",
+        "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
+        "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        SKPaymentQueue.default().add(self)
+
+        if isPurchased() {
+            showPremiumQuotes()
+        }
+       
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if isPurchased() {
+            return quotesToShow.count
+        } else {
+            return quotesToShow.count + 1
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+
+        // Configure the cell...
+        
+        if indexPath.row < quotesToShow.count {
+        cell.textLabel?.text = quotesToShow[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            cell.accessoryType = .none
+        } else {
+            cell.textLabel?.text = "Get More Quotes"
+            cell.textLabel?.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            cell.accessoryType = .disclosureIndicator
+        }
+        return cell
+    }
+
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == quotesToShow.count {
+            buyPremiumQuotes()
+            
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+        
+    }
+
+    
+    //MARK: - In App purchase Methods
+    
+    func buyPremiumQuotes() {
+        if SKPaymentQueue.canMakePayments() {
+            // Can make payments
+            
+            let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = productID
+            SKPaymentQueue.default().add(paymentRequest)
+            
+            
+        } else {
+            //cant make payments
+            print("User Cant Make Payment")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            if transaction.transactionState == .purchased {
+                // User payment successfull
+                print("Transaction successful!l")
+                
+                showPremiumQuotes()
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+                
+            } else if transaction.transactionState == .failed {
+                // Payment Failed
+               
+                if let error = transaction.error {
+                    let errorDescription = error.localizedDescription
+                     print("Transaction Failed due to error: \(errorDescription)")
+                }
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+            } else if transaction.transactionState == .restored {
+                
+                showPremiumQuotes()
+                
+                print("Transaction is restored")
+                
+                SKPaymentQueue.default().finishTransaction(transaction)
+                
+                navigationItem.setRightBarButtonItems(nil, animated: true)
+            }
+        }
+    }
+    
+    func showPremiumQuotes() {
+        
+        UserDefaults.standard.set(true, forKey: productID)
+        
+        quotesToShow.append(contentsOf: premiumQuotes)
+        tableView.reloadData()
+        
+    }
+    
+    func isPurchased() -> Bool {
+        let purchasedStatus = UserDefaults.standard.bool(forKey: productID)
+        
+        if purchasedStatus {
+            print("previously purchased")
+            return true
+        } else {
+            print("Never purchased")
+            return false
+        }
+    }
+
+}
